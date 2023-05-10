b065e84e25cead14012948af17851354523cae6b
wac <wabaham9@gmail.com>
2023-05-10 12:22:31 +0000

    tasks 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,9 +11,18 @@ Type-annotate store correctly. Remember that data can be a str, bytes,
 int or float'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
+def count_calls(self, method: Callable) -> Callable:
+    ''' count number of times this method is called '''
+    @wrapper(method)
+    def func(self, *args, **kwargs):
+        ''' rapper function '''
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return func
+    
 class Cache:
     '''
     Cache class that handles caching operation
@@ -32,3 +41,24 @@ class Cache:
         id1 = str(uuid.uuid4())
         self._redis.set(id1, data)
         return id1
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        ''' returns converted data or original byte '''
+        result = self._redis.get(key)
+        if not key or not result or not callable(fn):
+            return result
+        return fn(result)
+
+    def get_str(self, key: str) -> str:
+        ''' returns str version of key value '''
+        val = self._redis.get(key)
+        return val.decode('utf_8')
+    def get_int(self, data: str) -> int:
+        ''' returns int version of key value '''
+        val = self._redis.get(key)
+        try:
+            val = int(val.decode('utf-8'))
+        except Exception:
+            val = 0
+        return val
---
+++ 0x02-redis_basic/test.py
@@ -0,0 +1,12 @@
+Cache = __import__('exercise').Cache
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
