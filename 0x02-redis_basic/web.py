6f6c89c9b97e497a8c4e0ed8d2c6c9c992d6bed1
wac <wabaham9@gmail.com>
2023-05-10 17:23:38 +0000

    git push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,51 +1,36 @@
 #!/usr/bin/env python3
-''' In this tasks, we will implement a get_page function (prototype:
-def get_page(url: str) -> str:). The core of the function is very simple.
-It uses the requests module to obtain the HTML content of a particular
-URL and returns it.
-
-Start in a new file named web.py and do not reuse the code written in
-exercise.py.
-
-Inside get_page track how many times a particular URL was accessed in
-the key "count:{url}" and cache the result with an expiration time of
-10 seconds.
-
-Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
-and test your caching.
-
-Bonus: implement this use case with decorators.
-'''
+"""
+web cache and tracker
+"""
 import requests
 import redis
 from functools import wraps
-from typing import Callable
+
+store = redis.Redis()
 
 
-db = redis.Redis()
-
-
-def count_call(method: Callable) -> Callable:
-    ''' count number of time a website is accessed '''
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def function(url):
-        ''' wrap function '''
-        ckey = 'cached:' + url
-        cdata = db.get(ckey)
-        if cdata:
-            return cdata.decode('utf-8')
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
 
-        key = 'count:' + url
-        db.incr(key)
-        page = method(url)
-        db.set(ckey, page)
-        db.expire(ckey, 10)
-        return page
-    return function
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
-@count_call
+@count_url_access
 def get_page(url: str) -> str:
-    ''' fetch content from url '''
-    db = redis.Redis()
-    return requests.get(url).text
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
